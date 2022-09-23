a1adb53bd1f29856b252d6dc1926188a0e0ac3ce
anotherRedbeard <andrewredman@Redbeard-MBP.local>
2022-09-23 16:39:00 -0500

    update cors

--- Program.cs
+++ Program.cs
@@ -26,8 +26,7 @@ app.UseHttpsRedirection();
 
 //policy.WithOrigins("https://localhost:7270","http://localhost:5118","https://win-wire-app--8nfao0p.redgrass-633dc5ff.eastus.azurecontainerapps.io")
 app.UseCors(policy => policy
-    //.WithOrigins("https://localhost:7139")
-    .AllowAnyOrigin()
+    .WithOrigins("https://red-scus-blazorclient-aca.icymoss-1ea051d6.southcentralus.azurecontainerapps.io")
     .AllowAnyMethod()
     .WithHeaders(Microsoft.Net.Http.Headers.HeaderNames.ContentType)
 );
