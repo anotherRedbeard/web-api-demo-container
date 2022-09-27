f1731a77a1f6b9c51c73e16597aae189ad860e9e
anotherRedbeard <andrewredman@Redbeard-MBP.local>
2022-09-27 16:30:39 -0500

    update cors...again

--- Program.cs
+++ Program.cs
@@ -28,8 +28,7 @@ var corsAllowedHosts = builder.Configuration.GetSection("CorsAllowedHosts").GetC
 
 //policy.WithOrigins("https://localhost:7270","http://localhost:5118","https://win-wire-app--8nfao0p.redgrass-633dc5ff.eastus.azurecontainerapps.io")
 app.UseCors(policy => policy
-    //.WithOrigins(corsAllowedHosts)
-    .AllowAnyOrigin()
+    .WithOrigins(corsAllowedHosts)
     .AllowAnyMethod()
     .WithHeaders(Microsoft.Net.Http.Headers.HeaderNames.ContentType)
 );
