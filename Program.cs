a8b9c51899ddb3f5255478bf3e3eaec29bb7a2ca
anotherRedbeard <andrewredman@Redbeard-MBP.local>
2022-09-27 16:41:47 -0500

    add debug message

--- Program.cs
+++ Program.cs
@@ -25,6 +25,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 var corsAllowedHosts = builder.Configuration.GetSection("CorsAllowedHosts").GetChildren().Select(x => x.Value).ToArray();
+Console.WriteLine($"CorsAllowed:  {string.Join(",",corsAllowedHosts)}!");
 
 //policy.WithOrigins("https://localhost:7270","http://localhost:5118","https://win-wire-app--8nfao0p.redgrass-633dc5ff.eastus.azurecontainerapps.io")
 app.UseCors(policy => policy
