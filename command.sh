62cccbfc680727fd0be95c79aabb591eff07799d
anotherRedbeard <andrewredman@Redbeard-MBP.local>
2022-11-14 13:53:10 -0600

    update aks wf to use build-push-action

--- command.sh
+++ command.sh
@@ -1,25 +1,25 @@
-//these are all the commands I used to get everything deployed
+#these are all the commands I used to get everything deployed
 
-//create log analytics workspace
+#create log analytics workspace
 az monitor log-analytics workspace create --resource-group ${{ env.resource-group-name }} \
   --workspace-name ${{ env.log-analytics-workspace-name }}
 
-//create acr
+#create acr
 az acr create -n <registry_name> -g <resource_group_name> --sku Standard
 
-//connect to ACR
+#connect to ACR
 az acr login --name <registry_name>
 
-//push image to registry_name
+#push image to registry_name
 docker push <registry_url>/<image_name>:<version>
 
-//create new aca environment
+#create new aca environment
 az containerapp env create \
   --name $CONTAINERAPPS_ENVIRONMENT \
   --resource-group $RESOURCE_GROUP \
   --location $LOCATION
 
-//create new aca 
+#create new aca 
 az containerapp create \
   --name my-container-app \
   --resource-group $RESOURCE_GROUP \
@@ -30,5 +30,5 @@ az containerapp create \
   --registry-password $REGISTRY_PASSWORD
   --secrets "queue-connection-string=$CONNECTION_STRING"
 
-//deploys the dockerfile in the . source folder into the ACA environment
+#deploys the dockerfile in the . source folder into the ACA environment
 az containerapp up -n red-scus-demo-aca --environment dev -g red-scus-fun-with-containers-rg -l southcentralus --source .
